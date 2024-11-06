be209d7ff2d6458f4889f6aff5451e8f5c85dc8c
gleuber.souza <gleuber.souza@dbserver.com.br>
2024-11-06 20:29:22 -0300

    Colocado size no DTO de pauta e adicionado o arquivo de importação para o postman das API's

--- src/main/java/com/desafio_votacao/api/model/pauta/dto/PautaRequestDto.java
+++ src/main/java/com/desafio_votacao/api/model/pauta/dto/PautaRequestDto.java
@@ -1,13 +1,16 @@
 package com.desafio_votacao.api.model.pauta.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PautaRequestDto {
 
     @NotBlank(message = "O título é obrigatório.")
+    @Size(max = 255, message = "O título não pode ter mais de 255 caracteres.")
     private String titulo;
 
+    @Size(max = 255, message = "A descrição não pode ter mais de 255 caracteres.")
     private String descricao;
 }
